614640a6f186931896bef53fac5fc404ffedf290
DenisGhg <denisdenos5837@gmail.com>
2025-05-01 00:59:51 +0100

    Ajout des Couleurs à AppColors

--- lib/views/colors/app_colors.dart
+++ lib/views/colors/app_colors.dart
@@ -1,28 +1,13 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color primaryColor = const Color(0xFF03A63D);
-  static Color secondaryColor = const Color.fromARGB(255, 50, 131, 13);
-  static Color tertiaryColor = const Color.fromARGB(255, 49, 63, 58);
-  static Color blueColor = const Color(0xFF2196F3);
-  static Color redColor = const Color.fromARGB(179, 255, 0, 0);
-  static Color yellowColor = const Color.fromARGB(179, 255, 255, 0);
-  static Color orangeColor = const Color.fromARGB(179, 255, 165, 0);
-  static Color purpleColor = const Color.fromARGB(179, 128, 0, 128);
+  static Color blueColor = const Color.fromARGB(255, 33, 150, 243);
+  static Color redColor = const Color.fromARGB(255, 244, 67, 54);
+  static Color greenColor = const Color.fromARGB(255, 76, 175, 80);
+  static Color greyColor = const Color.fromARGB(255, 158, 158, 158);
   static Color whiteColor = const Color.fromARGB(255, 255, 255, 255);
+  static Color orangeColor = const Color.fromARGB(255, 255, 152, 0);
+  static Color blueColorSecond = const Color.fromARGB(255, 13, 71, 161);
+  static Color blackColor = const Color.fromARGB(255, 0, 0, 0);
 }
 
-// TODO : A supprimer
-@Deprecated("Utiliser la classe AppColors à la place : AppColors.primaryColor")
-Color primaryColor =
-    const Color(0xFF03A63D); //const Color.fromARGB(255, 47, 155, 0);
-
-@Deprecated(
-    "Utiliser la classe AppColors à la place : AppColors.secondaryColor")
-Color secondaryColor = const Color.fromARGB(255, 50, 131, 13);
-
-@Deprecated("Utiliser la classe AppColors à la place : AppColors.tertiaryColor")
-Color tertiaryColor = const Color.fromARGB(255, 49, 63, 58);
-
-@Deprecated("Utiliser la classe AppColors à la place : AppColors.blueColor")
-Color blueColor = const Color.fromARGB(179, 0, 96, 216);
