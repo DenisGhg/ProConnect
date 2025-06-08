5e0930576f4e9e68a533bd27dca6a87fe9118215
DenisGhg <denisdenos5837@gmail.com>
2025-06-08 23:23:00 +0100

    Ajout d'effet d'animation à AppButton

--- lib/widgets/app_button.dart
+++ lib/widgets/app_button.dart
@@ -3,7 +3,7 @@ import 'package:pro_connect_projet/views/sizes/app_sizes.dart';
 
 import 'app_text.dart';
 
-class AppButton extends StatelessWidget {
+class AppButton extends StatefulWidget {
   final Function()? onTap;
   final double? height;
   final double? width;
@@ -30,23 +30,63 @@ class AppButton extends StatelessWidget {
   });
 
   @override
+  State<AppButton> createState() => _AppButtonState();
+}
+
+class _AppButtonState extends State<AppButton> with SingleTickerProviderStateMixin {
+  double _scale = 1.0;
+
+  void _onTapDown(TapDownDetails details) {
+    setState(() => _scale = 0.95);
+  }
+
+  void _onTapUp(TapUpDetails details) {
+    setState(() => _scale = 1.0);
+  }
+
+  void _onTapCancel() {
+    setState(() => _scale = 1.0);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final double height = widget.height ?? context.adjustAppSize(context.screenHeight, 0.06);
+    final double width = widget.width ?? context.adjustAppSize(context.screenWidth, 0.943);
+    final EdgeInsetsGeometry padding = widget.padding ?? EdgeInsets.all(context.screenWidth * 0.027);
+
     return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        height: height ?? context.adjustAppSize(context.screenHeight, 0.06),
-        width: width ?? context.adjustAppSize(context.screenWidth, 0.943),
-        alignment: alignment ?? Alignment.center,
-        padding: padding ?? EdgeInsets.all(context.screenWidth * 0.027),
-        decoration: BoxDecoration(
-          color: backgroundColor ?? Colors.blue,
-          borderRadius: BorderRadius.circular(radius ?? context.screenHeight * 0.027),
-          border: Border.all(
-            color: borderColor ?? Colors.transparent,
-            width: borderWidth ?? 0,
-          )
+      onTapDown: _onTapDown,
+      onTapUp: _onTapUp,
+      onTapCancel: _onTapCancel,
+      onTap: widget.onTap,
+      child: AnimatedScale(
+        scale: _scale,
+        duration: Duration(milliseconds: 100),
+        curve: Curves.easeInOut,
+        child: Container(
+          height: height,
+          width: width,
+          alignment: widget.alignment ?? Alignment.center,
+          padding: padding,
+          decoration: BoxDecoration(
+            color: widget.backgroundColor ?? Colors.blue,
+            borderRadius: BorderRadius.circular(widget.radius ?? context.screenHeight * 0.027),
+            border: Border.all(
+              color: widget.borderColor ?? Colors.transparent,
+              width: widget.borderWidth ?? 0,
+            ),
+            boxShadow: _scale < 1
+                ? []
+                : [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                offset: Offset(0, 4),
+                blurRadius: 6,
+              )
+            ],
+          ),
+          child: widget.child ?? AppText(text: ""),
         ),
-        child: child ?? AppText(text: ""),
       ),
     );
   }
